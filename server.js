a66dfbf991d8e42a6b15f8f63f95924f20f926ef
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2021-02-21 15:21:50 +0200

    lab06

--- server.js
+++ server.js
@@ -8,8 +8,42 @@ let express = require('express');
 let app = express();
 // for .env file 
 require('dotenv').config();
+
+const cors = require('cors');
+app.use(cors());
+
 // gor var from .env
 const PORT = process.env.PORT;
+
+// routes - endpoints
+app.get('/location',handlelocation);
+
+// handle function
+function handlelocation(req,res){
+    let searchQuery = req.query.city;
+    let locationObject = getLocationData(searchQuery);
+    res.status(200).send(locationObject);
+}
+
+//handle data for function
+function getLocationData(searchQuery){
+    //get array from json
+    let locationData = require('./data/location.json');
+    //get values from objects
+    let longitude = locationData[0].lon;
+    let latitude = locationData[0].lat;
+    let displayName = locationData[0].display_name;
+    let responseObject =new CityLocation(searchQuery,displayName,latitude,longitude);
+    return responseObject;
+}
+
+//constructors
+function CityLocation(searchQuery,displayName,lat,lon){
+this.search_query=searchQuery;
+this.formatted_query=displayName;
+this.latitude=lat;
+this.longitude=lon;
+}
 
 
 app.listen(PORT, ()=>{
