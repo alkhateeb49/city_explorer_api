e8970643f46025eaecf98df1ff7a9ebbec182e15
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2021-02-21 16:25:38 +0200

    lab06

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT;
 
 // routes - endpoints
 app.get('/location',handlelocation);
+app.get('/weather',handleweather);
 
 // handle function
 function handlelocation(req,res){
@@ -24,6 +25,15 @@ function handlelocation(req,res){
     let locationObject = getLocationData(searchQuery);
     res.status(200).send(locationObject);
 }
+
+function handleweather(req,res){
+    let searchQuery = req.query.city;
+    let weatherObject = getWeatherData(searchQuery);
+    res.status(200).send(weatherObject);
+}
+
+
+
 
 //handle data for function
 function getLocationData(searchQuery){
@@ -37,12 +47,35 @@ function getLocationData(searchQuery){
     return responseObject;
 }
 
+
+function getWeatherData(searchQuery){
+    // get data array from json 
+    let weatherData = require('./data/weather.json'); 
+    // get value from array 
+    let weatherArray = [];
+    for(let i = 0 ; i <weatherData.data.length; i++ ){
+        let weatherDesc = weatherData.data[i].weather['description'];
+        let time = weatherData.data[i].datetime;
+        time = time.replace("-","/");
+        let date = new Date(time).toString().slice(" ",16);
+                
+        let responseObject = new Cityweather(weatherDesc, date);
+        weatherArray.push(responseObject);
+    }
+    return weatherArray;  
+}
+
+
 //constructors
 function CityLocation(searchQuery,displayName,lat,lon){
 this.search_query=searchQuery;
 this.formatted_query=displayName;
 this.latitude=lat;
 this.longitude=lon;
+}
+function Cityweather(forecast,time){
+    this.forecast=forecast;
+    this.time=time;
 }
 
 
