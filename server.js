0a6644bc1b7772bd7847131c3863773b29a907e1
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2021-02-22 21:12:36 +0200

    last

--- server.js
+++ server.js
@@ -21,38 +21,36 @@ app.use(cors());
 const PORT = process.env.PORT;
 
 // routes - endpoints
-app.get('/location',handlelocation);
-app.get('/weather',handleweather);
-app.get('/parks',handleparks);
+app.get('/location', handlelocation);
+app.get('/weather', handleweather);
+app.get('/parks', handleparks);
 
 // handle function
-function handlelocation(req,res){
-  try{
+function handlelocation(req, res) {
+  try {
     let searchQuery = req.query.city;
-    getLocationData(searchQuery,res);
-  }catch(error){
+    getLocationData(searchQuery, res);
+  } catch (error) {
     res.status(500).send('Sorry, something went wron' + error);
   }
 }
 
-function handleweather(req,res){
-  try{
+function handleweather(req, res) {
+  try {
     let searchQuery = req.query.search_query;
-    let longitude =req.query.longitude;
+    let longitude = req.query.longitude;
     let latitude = req.query.latitude;
-    getWeatherData(searchQuery,longitude,latitude,res);
-  }catch(error){
+    getWeatherData(searchQuery, longitude, latitude, res);
+  } catch (error) {
     res.status(500).send('Sorry, something went wron' + error);
   }
 }
 
-function handleparks(req,res){
-  try{
+function handleparks(req, res) {
+  try {
     let searchQuery = req.query.search_query;
-    let longitude =req.query.longitude;
-    let latitude = req.query.latitude;
-    getParksData(searchQuery,res);
-  }catch(error){
+    getParksData(searchQuery, res);
+  } catch (error) {
     res.status(500).send('Sorry, something went wron' + error);
   }
 }
@@ -61,81 +59,81 @@ function handleparks(req,res){
 
 
 //handle data for function
-function getLocationData(searchQuery,res){
-  const query ={
-    key:process.env.GEOCODE_API_KEY,
-    q:searchQuery,
-    limit:1,
-    format:'json'
+function getLocationData(searchQuery, res) {
+  const query = {
+    key: process.env.GEOCODE_API_KEY,
+    q: searchQuery,
+    limit: 1,
+    format: 'json'
   };
-  let url='https://us1.locationiq.com/v1/search.php';
-  superagent.get(url).query(query).then(data =>{
-    try{
+  let url = 'https://us1.locationiq.com/v1/search.php';
+  superagent.get(url).query(query).then(data => {
+    try {
       let longitude = data.body[0].lon;
       let latitude = data.body[0].lat;
       let displayName = data.body[0].display_name;
       let responseObject = new CityLocation(searchQuery, displayName, latitude, longitude);
       res.status(200).send(responseObject);
-    }catch(error){
+    } catch (error) {
       res.status(500).send('Sorry, something went wron' + error);
     }
 
-  }).catch(error=>{
+  }).catch(error => {
     res.status(500).send('Sorry, something went wron' + error);
   });
 
 }
 
 
-function getWeatherData(searchQuery,longitude,latitude,res){
-  const weatherquery ={
-    key:process.env.WEATHER_API_KEY,
-    city:searchQuery,
-    longitude:longitude,
-    latitude:latitude,
-    days:8
+function getWeatherData(searchQuery, longitude, latitude, res) {
+  const weatherquery = {
+    key: process.env.WEATHER_API_KEY,
+    city: searchQuery,
+    longitude: longitude,
+    latitude: latitude,
+    days: 8
   };
-//   let url ='http://api.weatherbit.io/v2.0/current';
-  let url ='http://api.weatherbit.io/v2.0/forecast/daily';
-  superagent.get(url).query(weatherquery).then(data =>{
-    try{
-    //   res.status(200).send(data["count"]);
-    //   res.status(200).send(JSON.parse(data.text).data[0]);
-    //   res.status(200).send(JSON.parse(data.text).data[0].weather.description);
-    //   console.log(JSON.parse(data.text).data[0].weather.description);
-      let weatherArray =[];
-      for(let i = 0 ; i <8; i++ ){
-      let weatherDesc =JSON.parse(data.text).data[i].weather.description;
-      let date = JSON.parse(data.text).data[i].datetime;
-      let responseObject = new Cityweather(weatherDesc, date);
-      weatherArray.push(responseObject);
+  //   let url ='http://api.weatherbit.io/v2.0/current';
+  let url = 'http://api.weatherbit.io/v2.0/forecast/daily';
+  superagent.get(url).query(weatherquery).then(data => {
+    try {
+      //   res.status(200).send(data["count"]);
+      //   res.status(200).send(JSON.parse(data.text).data[0]);
+      //   res.status(200).send(JSON.parse(data.text).data[0].weather.description);
+      //   console.log(JSON.parse(data.text).data[0].weather.description);
+      let weatherArray = [];
+      for (let i = 0; i < 8; i++) {
+        let weatherDesc = JSON.parse(data.text).data[i].weather.description;
+        let date = JSON.parse(data.text).data[i].datetime;
+        let responseObject = new Cityweather(weatherDesc, date);
+        weatherArray.push(responseObject);
       }
       res.status(200).send(weatherArray);
-    //   console.log(weatherArray);
-    }catch(error){
+      //   console.log(weatherArray);
+    } catch (error) {
       res.status(500).send('Sorry, something went wron' + error);
     }
 
-  }).catch(error=>{
+  }).catch(error => {
     res.status(500).send('Sorry, something went wron' + error);
   });
 
 
 
-// // get data array from json 
-// let weatherData = require('./data/weather.json'); 
-// // get value from array 
-// let weatherArray = [];
-// for(let i = 0 ; i <weatherData.data.length; i++ ){
-//     let weatherDesc = weatherData.data[i].weather['description'];
-//     let time = weatherData.data[i].datetime;
-//     time = time.replace("-","/");
-//     let date = new Date(time).toString().slice(" ",16);
-    
-//     let responseObject = new Cityweather(weatherDesc, date);
-//     weatherArray.push(responseObject);
-// }
-// return weatherArray;  
+  // // get data array from json 
+  // let weatherData = require('./data/weather.json'); 
+  // // get value from array 
+  // let weatherArray = [];
+  // for(let i = 0 ; i <weatherData.data.length; i++ ){
+  //     let weatherDesc = weatherData.data[i].weather['description'];
+  //     let time = weatherData.data[i].datetime;
+  //     time = time.replace("-","/");
+  //     let date = new Date(time).toString().slice(" ",16);
+
+  //     let responseObject = new Cityweather(weatherDesc, date);
+  //     weatherArray.push(responseObject);
+  // }
+  // return weatherArray;  
 }
 
 
@@ -187,7 +185,7 @@ function getParksData(searchQuery, res) {
         arrayOfObjects.push(responseObject);
       });
       res.status(200).send(arrayOfObjects);
-    } catch(error){
+    } catch (error) {
       res.status(500).send("Sorry, something went wrong");
     }
   }).catch((error) => {
@@ -198,25 +196,25 @@ function getParksData(searchQuery, res) {
 
 
 //constructors
-function CityLocation(searchQuery,displayName,lat,lon){
-this.search_query=searchQuery;
-this.formatted_query=displayName;
-this.latitude=lat;
-this.longitude=lon;
+function CityLocation(searchQuery, displayName, lat, lon) {
+  this.search_query = searchQuery;
+  this.formatted_query = displayName;
+  this.latitude = lat;
+  this.longitude = lon;
 }
-function Cityweather(forecast,time){
-    this.forecast=forecast;
-    this.time=time;
+function Cityweather(forecast, time) {
+  this.forecast = forecast;
+  this.time = time;
 }
-function CityParks(name, address,fee,description, url){
-    this.name=name;
-    this.address=address;
-    this.fee=fee;
-    this.description=description;
-    this.url=url;
+function CityParks(name, address, fee, description, url) {
+  this.name = name;
+  this.address = address;
+  this.fee = fee;
+  this.description = description;
+  this.url = url;
 }
 
 
-app.listen(PORT, ()=>{
-    console.log('the app is listening to '+ PORT);
+app.listen(PORT, () => {
+  console.log('the app is listening to ' + PORT);
 });
